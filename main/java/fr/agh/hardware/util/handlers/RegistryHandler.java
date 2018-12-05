0ca4c5d7435bb8b961d2e42702e739e348437ef2
PrSnake <38890802+PrSnake@users.noreply.github.com>
2018-12-05 21:18:34 +0100

    Update RegistryHandler.java

--- main/java/fr/agh/hardware/util/handlers/RegistryHandler.java
+++ main/java/fr/agh/hardware/util/handlers/RegistryHandler.java
@@ -23,12 +23,12 @@ public class RegistryHandler {
 	
 	@SubscribeEvent
 	public static void onItemRegister(RegistryEvent.Register<Item> event) {
-		event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));;
+		event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
 	}
 	
 	@SubscribeEvent
 	public static void onBlockRegister(RegistryEvent.Register<Block> event) {
-		event.getRegistry().registerAll(BlockInit.BLOCKS.toArray(new Block[0]));;
+		event.getRegistry().registerAll(BlockInit.BLOCKS.toArray(new Block[0]));
 	}
 	
 	@SubscribeEvent

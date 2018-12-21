fcc5d160dcff89b75ed84e754e129d6e1ea8e012
PrSnake <38890802+PrSnake@users.noreply.github.com>
2018-12-21 21:39:35 +0100

    Ore crushing facing the good way !

--- main/java/fr/agh/hardware/objects/blocks/BlockOreCrusher.java
+++ main/java/fr/agh/hardware/objects/blocks/BlockOreCrusher.java
@@ -48,149 +48,120 @@ public class BlockOreCrusher extends BlockBase {
 	/**
 	* Called serverside after this block is replaced with another in Chunk, but before the Tile Entity is updated
 	*/
-	public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
-	{
-	    if (!keepInventory)
-            {
-                TileEntity tileentity = worldIn.getTileEntity(pos);
-    
-                if (tileentity instanceof TileEntityFurnace)
-                {
-                    InventoryHelper.dropInventoryItems(worldIn, pos, (TileEntityFurnace)tileentity);
-                    worldIn.updateComparatorOutputLevel(pos, this);
-                }
-            }
-
-            super.breakBlock(worldIn, pos, state);
-        }
+	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+		if (!keepInventory) {
+			TileEntity tileentity = worldIn.getTileEntity(pos);
+			if (tileentity instanceof TileEntityFurnace) {
+				InventoryHelper.dropInventoryItems(worldIn, pos, (TileEntityFurnace)tileentity);
+				worldIn.updateComparatorOutputLevel(pos, this);
+			}
+		}
+		super.breakBlock(worldIn, pos, state);
+	}
 	
 	@Override
-	public Item getItemDropped(IBlockState state, Random rand, int fortune) 
-	{
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
 		return Item.getItemFromBlock(BlockInit.MACHINES_ORE_CRUSHER);
 	}
 	
 	@Override
-	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
-	{
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
 		return new ItemStack(BlockInit.MACHINES_ORE_CRUSHER);
 	}
 	
 	@Override
-	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) 
-	{
-		if(!worldIn.isRemote)
-		{
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if(!worldIn.isRemote) {
 			playerIn.openGui(ModHardware.instance, HardwareReference.GUI_ORE_CRUSHER, worldIn, pos.getX(), pos.getY(), pos.getZ());
 		}
-		
 		return true;
 	}
 	
 	@Override
-	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) 
-	{
-		if (!worldIn.isRemote) 
-        {
-            IBlockState north = worldIn.getBlockState(pos.north());
-            IBlockState south = worldIn.getBlockState(pos.south());
-            IBlockState west = worldIn.getBlockState(pos.west());
-            IBlockState east = worldIn.getBlockState(pos.east());
-            EnumFacing face = (EnumFacing)state.getValue(FACING);
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+		if (!worldIn.isRemote) {
+			IBlockState north = worldIn.getBlockState(pos.north());
+			IBlockState south = worldIn.getBlockState(pos.south());
+			IBlockState west = worldIn.getBlockState(pos.west());
+			IBlockState east = worldIn.getBlockState(pos.east());
+			EnumFacing face = (EnumFacing)state.getValue(FACING);
 
-            if (face == EnumFacing.NORTH && north.isFullBlock() && !south.isFullBlock()) face = EnumFacing.SOUTH;
-            else if (face == EnumFacing.SOUTH && south.isFullBlock() && !north.isFullBlock()) face = EnumFacing.NORTH;
-            else if (face == EnumFacing.WEST && west.isFullBlock() && !east.isFullBlock()) face = EnumFacing.EAST;
-            else if (face == EnumFacing.EAST && east.isFullBlock() && !west.isFullBlock()) face = EnumFacing.WEST;
-            worldIn.setBlockState(pos, state.withProperty(FACING, face), 2);
-        }
+			if (face == EnumFacing.NORTH && north.isFullBlock() && !south.isFullBlock()) face = EnumFacing.SOUTH;
+			else if (face == EnumFacing.SOUTH && south.isFullBlock() && !north.isFullBlock()) face = EnumFacing.NORTH;
+			else if (face == EnumFacing.WEST && west.isFullBlock() && !east.isFullBlock()) face = EnumFacing.EAST;
+			else if (face == EnumFacing.EAST && east.isFullBlock() && !west.isFullBlock()) face = EnumFacing.WEST;
+			worldIn.setBlockState(pos, state.withProperty(FACING, face), 2);
+		}
 	}
-	
-	public static void setState(boolean active, World worldIn, BlockPos pos) 
-	{
+
+	public static void setState(boolean active, World worldIn, BlockPos pos) {
 		IBlockState state = worldIn.getBlockState(pos);
 		TileEntity tileentity = worldIn.getTileEntity(pos);
 		keepInventory = true;
 		
-		if(active)
-		{
+		if(active) {
 			worldIn.setBlockState(pos, BlockInit.MACHINES_ORE_CRUSHER.getDefaultState().withProperty(FACING, state.getValue(FACING)).withProperty(BURNING, true), 3);
-		}
-		else
-		{
+		} else {
 			worldIn.setBlockState(pos, BlockInit.MACHINES_ORE_CRUSHER.getDefaultState().withProperty(FACING, state.getValue(FACING)).withProperty(BURNING, false), 3);
 		}
-		
 		keepInventory = false;
-		
-		if(tileentity != null) 
-		{
+		if(tileentity != null) {
 			tileentity.validate();
 			worldIn.setTileEntity(pos, tileentity);
 		}
 	}
 	
 	@Override
-	public boolean hasTileEntity(IBlockState state) 
-	{
+	public boolean hasTileEntity(IBlockState state) {
 		return true;
 	}
 	
 	@Override
-	public TileEntity createTileEntity(World world, IBlockState state) 
-	{
+	public TileEntity createTileEntity(World world, IBlockState state) {
 		return new TileEntityOreCrusher();
 	}
 	
 	@Override
-	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) 
-	{
+	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
 		return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
 	}
 	
 	@Override
-	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) 
-	{
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
 		worldIn.setBlockState(pos, this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
 	}
 	
 	@Override
-	public EnumBlockRenderType getRenderType(IBlockState state) 
-	{
+	public EnumBlockRenderType getRenderType(IBlockState state) {
 		return EnumBlockRenderType.MODEL;
 	}
 	
 	@Override
-	public IBlockState withRotation(IBlockState state, Rotation rot)
-	{
+	public IBlockState withRotation(IBlockState state, Rotation rot) {
 		return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
 	}
 	
 	@Override
-	public IBlockState withMirror(IBlockState state, Mirror mirrorIn) 
-	{
+	public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
 		return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(FACING)));
 	}
 	
 	@Override
-	protected BlockStateContainer createBlockState() 
-	{
+	protected BlockStateContainer createBlockState() {
 		return new BlockStateContainer(this, new IProperty[] {BURNING,FACING});
 	}
 	
-	/* Deprecated
+	//Deprecated in it's actual form, need to replace EnumFacing.getFront(meta) and adapt the rest of the method
 	@Override
 	public IBlockState getStateFromMeta(int meta) 
 	{
-		//EnumFacing facing = EnumFacing.getFront(meta);
-		EnumFacing facing = EnumFacing.getAxisDirection();
+		EnumFacing facing = EnumFacing.byIndex(meta);
 		if(facing.getAxis() == EnumFacing.Axis.Y) facing = EnumFacing.NORTH;
 		return this.getDefaultState().withProperty(FACING, facing);
-	}*/
+	}
 	
 	@Override
-	public int getMetaFromState(IBlockState state) 
-	{
+	public int getMetaFromState(IBlockState state) {
 		return ((EnumFacing)state.getValue(FACING)).getIndex();
 	}
 

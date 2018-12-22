0cc8f10be1ff8c4196584d149d6034eb9f56e1f0
PrSnake <38890802+PrSnake@users.noreply.github.com>
2018-12-22 17:49:07 +0100

    Added new creative tab
    added a creative tab for machines upgrade

--- main/java/fr/agh/hardware/init/ItemInit.java
+++ main/java/fr/agh/hardware/init/ItemInit.java
@@ -13,7 +13,7 @@ public class ItemInit {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
 	// Minerai brut
-	// public static final Item RAW_ = new ItemBase("item_raw_", ModHardware.MINERALS_TAB);
+	// public static final Item RAW_ = new ItemBase("item_raw_", CreativeTabInit.TAB_MINERALS);
 	public static final Item RAW_COPPER = new ItemBase("item_raw_copper", CreativeTabInit.TAB_MINERALS);
 	public static final Item RAW_COBALT = new ItemBase("item_raw_cobalt", CreativeTabInit.TAB_MINERALS);
 	public static final Item RAW_BAUXITE = new ItemBase("item_raw_bauxite", CreativeTabInit.TAB_MINERALS);
@@ -21,7 +21,7 @@ public class ItemInit {
 	public static final Item RAW_VANADIUM = new ItemBase("item_raw_vanadium", CreativeTabInit.TAB_MINERALS);
 	
 	// Lingots
-	// public static final Item INGOT_ = new ItemBase("item_ingot_", ModHardware.MINERALS_TAB);
+	// public static final Item INGOT_ = new ItemBase("item_ingot_", CreativeTabInit.TAB_MINERALS);
 	public static final Item INGOT_COPPER = new ItemBase("item_ingot_copper", CreativeTabInit.TAB_MINERALS);
 	public static final Item INGOT_COBALT = new ItemBase("item_ingot_cobalt", CreativeTabInit.TAB_MINERALS);
 	public static final Item INGOT_ALUMINIUM = new ItemBase("item_ingot_aluminium", CreativeTabInit.TAB_MINERALS);
@@ -29,9 +29,15 @@ public class ItemInit {
 	public static final Item INGOT_VANADIUM = new ItemBase("item_ingot_vanadium", CreativeTabInit.TAB_MINERALS);
 	
 	// Composant de machines
-	// public static final Item  = new ItemBase("item_", ModHardware.COMPONENTS_TAB);
+	// public static final Item  = new ItemBase("item_", CreativeTabInit.TAB_COMPONENTS);
 	public static final Item PLATE_IRON = new ItemBase("item_plate_iron", CreativeTabInit.TAB_COMPONENTS);
 	public static final Item RODE_IRON = new ItemBase("item_rode_iron", CreativeTabInit.TAB_COMPONENTS);
 	public static final Item GEARS_IRON = new ItemBase("item_gears_iron", CreativeTabInit.TAB_COMPONENTS);
 	public static final Item GRINDER_IRON = new ItemBase("item_grinder_iron", CreativeTabInit.TAB_COMPONENTS);
+	
+	// Upgrades
+	// public static final Item  = new ItemBase("item_", ModHardware.COMPONENTS_TAB);
+	public static final Item ORE_CRUSHER_UPGRADE_FUEL_EFFICIENCY = new ItemBase("item_ore_crusher_upgrade_fuel_efficiency", CreativeTabInit.TAB_COMPONENTS);
+	public static final Item ORE_CRUSHER_UPGRADE_PRODUCTION = new ItemBase("item_ore_crusher_upgrade_production", CreativeTabInit.TAB_COMPONENTS);
+	public static final Item ORE_CRUSHER_UPGRADE_SPEED = new ItemBase("item_ore_crusher_upgrade_speed", CreativeTabInit.TAB_COMPONENTS);
 }

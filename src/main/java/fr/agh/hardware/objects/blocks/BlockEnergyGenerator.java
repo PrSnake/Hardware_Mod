ad23999af5533e13c1410bce31955b77f79ec3d1
PrSnake <38890802+PrSnake@users.noreply.github.com>
2019-03-02 22:14:54 +0100

    EnergyGenerator
    WIP first part

---
+++ src/main/java/fr/agh/hardware/objects/blocks/BlockEnergyGenerator.java
@@ -0,0 +1,190 @@
+package fr.agh.hardware.objects.blocks;
+
+import java.util.Random;
+
+import fr.agh.hardware.ModHardware;
+import fr.agh.hardware.init.BlockInit;
+import fr.agh.hardware.objects.blocks.tileentities.TileEntityEnergyGenerator;
+import fr.agh.hardware.util.HardwareReference;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+
+public class BlockEnergyGenerator extends BlockBase{
+	
+	public static final PropertyDirection FACING = BlockHorizontal.FACING;
+	public static final PropertyBool BURNING = PropertyBool.create("burning");
+
+	public BlockEnergyGenerator(String name,CreativeTabs tab) {
+		super(name, tab, Material.IRON, 5.0F, 3.0F);
+		
+		setSoundType(SoundType.METAL);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(BURNING, false));
+	}
+	
+	/**
+	* Called serverside after this block is replaced with another in Chunk, but before the Tile Entity is updated
+	*/
+	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+		TileEntityEnergyGenerator te = (TileEntityEnergyGenerator) worldIn.getTileEntity(pos);
+		IItemHandler handler = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
+		for (int slot = 0; slot < handler.getSlots(); slot++) {
+			ItemStack stack = handler.getStackInSlot(slot);
+			InventoryHelper.spawnItemStack(worldIn, pos.getX(), pos.getY(), pos.getZ(), stack);
+		}
+		super.breakBlock(worldIn, pos, state);
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		// TODO generic
+		return Item.getItemFromBlock(BlockInit.MACHINES_ENERGY_GENERATOR);
+	}
+	
+	@Override
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
+		// TODO generic
+		return new ItemStack(BlockInit.MACHINES_ENERGY_GENERATOR);
+	}
+	
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if (!worldIn.isRemote) {
+			playerIn.openGui(ModHardware.instance, HardwareReference.GUI_ENERGY_GENERATOR, worldIn, pos.getX(), pos.getY(), pos.getZ());
+		}
+		return true;
+	}
+	
+	@Override
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+		if (!worldIn.isRemote) {
+			IBlockState north = worldIn.getBlockState(pos.north());
+			IBlockState south = worldIn.getBlockState(pos.south());
+			IBlockState west = worldIn.getBlockState(pos.west());
+			IBlockState east = worldIn.getBlockState(pos.east());
+			EnumFacing face = state.getValue(FACING);
+
+			if (face == EnumFacing.NORTH && north.isFullBlock() && !south.isFullBlock()) face = EnumFacing.SOUTH;
+			else if (face == EnumFacing.SOUTH && south.isFullBlock() && !north.isFullBlock()) face = EnumFacing.NORTH;
+			else if (face == EnumFacing.WEST && west.isFullBlock() && !east.isFullBlock()) face = EnumFacing.EAST;
+			else if (face == EnumFacing.EAST && east.isFullBlock() && !west.isFullBlock()) face = EnumFacing.WEST;
+			worldIn.setBlockState(pos, state.withProperty(FACING, face), 2);
+		}
+	}
+
+	public static void setState(boolean active, World worldIn, BlockPos pos) {
+		IBlockState state = worldIn.getBlockState(pos);
+		TileEntity tileentity = worldIn.getTileEntity(pos);
+		
+		if(active) {
+			worldIn.setBlockState(pos, BlockInit.MACHINES_ENERGY_GENERATOR.getDefaultState().withProperty(FACING, state.getValue(FACING)).withProperty(BURNING, true), 3);
+		} else {
+			worldIn.setBlockState(pos, BlockInit.MACHINES_ENERGY_GENERATOR.getDefaultState().withProperty(FACING, state.getValue(FACING)).withProperty(BURNING, false), 3);
+		}
+		if(tileentity != null) {
+			tileentity.validate();
+			worldIn.setTileEntity(pos, tileentity);
+		}
+	}
+	
+	@Override
+	public boolean hasTileEntity(IBlockState state) {
+		return true;
+	}
+	
+	@Override
+	public TileEntity createTileEntity(World world, IBlockState state) {
+		// TODO generic
+		return new TileEntityEnergyGenerator();
+	}
+	
+	@Override
+	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+		return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+	}
+	
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+		worldIn.setBlockState(pos, this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+	}
+	
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
+	
+	@Override
+	public IBlockState withRotation(IBlockState state, Rotation rot) {
+		return state.withProperty(FACING, rot.rotate(state.getValue(FACING)));
+	}
+	
+	@Override
+	public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+		return state.withRotation(mirrorIn.toRotation(state.getValue(FACING)));
+	}
+	
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] {BURNING,FACING});
+	}
+	
+	//Deprecated in it's actual form, need to replace EnumFacing.getFront(meta) and adapt the rest of the method
+	@Override
+	public IBlockState getStateFromMeta(int meta) 
+	{
+		EnumFacing facing = EnumFacing.getFront(meta);
+		if(facing.getAxis() == EnumFacing.Axis.Y) facing = EnumFacing.NORTH;
+		return this.getDefaultState().withProperty(FACING, facing);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return (state.getValue(FACING)).getIndex();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
